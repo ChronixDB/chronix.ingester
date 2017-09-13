95a28ffb480d5e3ba8f6939a0bbce4f21aabb3e2
Julius Volz <julius.volz@gmail.com>
2017-09-13 16:45:57 +0200

    Update remote write protocol version to 0.1.0
    Fixes https://github.com/ChronixDB/chronix.ingester/issues/9
    Fixes https://github.com/ChronixDB/chronix.ingester/issues/10

--- handler.go
+++ handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -15,16 +16,30 @@ import (
 // ingestHandler returns an http.Handler that accepts proto encoded samples.
 func ingestHandler(appender storage.SampleAppender) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		reqBuf, err := ioutil.ReadAll(snappy.NewReader(r.Body))
+		if v := r.Header.Get("X-Prometheus-Remote-Write-Version"); v != "0.1.0" {
+			msg := fmt.Sprintf("Unsupported remote write protocol version %q", v)
+			log.Errorln(msg)
+			http.Error(w, msg, http.StatusBadRequest)
+			return
+		}
+
+		compressed, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			log.Errorf("error reading request body: %v", err)
+			log.Errorf("Error reading request body: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		reqBuf, err := snappy.Decode(nil, compressed)
+		if err != nil {
+			log.Errorf("Error decompressing request body: %v", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		var req remote.WriteRequest
 		if err := proto.Unmarshal(reqBuf, &req); err != nil {
-			log.Errorf("error unmarshalling request body: %v", err)
+			log.Errorf("Error unmarshalling request body: %v", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
@@ -42,7 +57,7 @@ func ingestHandler(appender storage.SampleAppender) http.Handler {
 					Timestamp: model.Time(s.TimestampMs),
 				})
 				if err != nil {
-					log.Errorf("error appending sample: %v", err)
+					log.Errorf("Error appending sample: %v", err)
 					http.Error(w, err.Error(), http.StatusInternalServerError)
 					return
 				}
